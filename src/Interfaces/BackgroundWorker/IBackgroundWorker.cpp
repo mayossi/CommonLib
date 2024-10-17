9d63cb79e4225d6f0fa1f4853ca6efac6f7b0d67
Yossi Mansharov <yossi17301@gmail.com>
2024-10-18 00:17:09 +0300

    Added RAII macros and BackgroundWorker interface

---
+++ src/Interfaces/BackgroundWorker/IBackgroundWorker.cpp
@@ -0,0 +1,21 @@
+#include "IBackgroundWorker.h"
+
+
+namespace clib::interfaces
+{
+	void IBackgroundWorker::start()
+	{
+		m_stopWorker = false;
+		m_workerThread = std::thread(&IBackgroundWorker::work, this);
+	}
+
+	void IBackgroundWorker::stop()
+	{
+		m_stopWorker = true;
+		if (m_workerThread.joinable())
+		{
+			m_workerThread.join();
+		}
+	}
+
+} // namespace clib::interfaces
